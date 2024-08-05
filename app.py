ef40d47336d0c3e5b18525b8d18d85af73a37d2d
Erinaldo <160548998+Erinaldo-IF@users.noreply.github.com>
2024-08-05 03:37:57 +0000

    add

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+from flask import flash, redirect
+
 
 app = Flask(__name__)
+app.config['SECRET_kEY'] = 'aluno123'
 
 @app.route('/')
 def index():
@@ -18,7 +22,15 @@ def cadastrarcidade():
 def resultado():
     return render_template('resultadocadastro.html')
 
-
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = request.form.get("usuario")
+    senha = request.form.get("senha")
+    if usuario != 'admin' or senha != 'senha123':
+       flash("Login ou senha incorretos")
+       return redirect("/login")
+    else:
+       	 return "Os dados recebidos foram: usuario = {} e senha = {}".format(usuario, senha)
 
 
 
