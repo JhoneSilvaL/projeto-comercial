9cb9c08e8bb772dfcaa07bd264b36886c815dbcf
Jhone Silva <145619969+JhoneSilvaL@users.noreply.github.com>
2024-09-19 02:13:18 +0000

    configurações feitas

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspaces/projeto-capitais/venv"
+set -gx VIRTUAL_ENV "/workspaces/projeto-comercial/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
