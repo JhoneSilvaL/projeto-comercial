ad7aa9f467e9ebac84248264a56ce26ce87f5cc2
Jhone Silva <145619969+JhoneSilvaL@users.noreply.github.com>
2024-09-19 21:56:07 +0000

    criando crud de categorias

--- controllers/categorias.py
+++ controllers/categorias.py
@@ -17,3 +17,36 @@ def create_categoria():
         else:
             flash('Nome da categoria não pode ser vazio!', 'danger')
     return render_template('categoria_create.html')  # Crie esse template
+
+@bp_categoria.route('/categoria/update/<int:id>', methods=['GET', 'POST'])
+def update_categoria(id):
+    categoria = Categoria.query.get_or_404(id)
+
+    if request.method == 'POST':
+        nome = request.form['nome']
+        if nome:
+            categoria.nome = nome
+            db.session.commit()
+            flash('Categoria atualizada com sucesso!', 'success')
+            return redirect(url_for('produto.recovery'))  # Redireciona onde desejar
+        else:
+            flash('Nome da categoria não pode ser vazio!', 'danger')
+
+    return render_template('categoria_update.html', categoria=categoria)
+
+@bp_categoria.route('/categoria/delete/<int:id>', methods=['GET', 'POST'])
+def delete_categoria(id):
+    categoria = Categoria.query.get_or_404(id)
+
+    if request.method == 'POST':
+        db.session.delete(categoria)
+        db.session.commit()
+        flash('Categoria deletada com sucesso!', 'success')
+        return redirect(url_for('produto.recovery'))  # Redireciona onde desejar
+
+    return render_template('categoria_delete.html', categoria=categoria)  # Crie esse template
+
+@bp_categoria.route('/categoria/recovery', methods=['GET'])
+def recovery_categoria():
+    categorias = Categoria.query.all()  # Recupera todas as categorias
+    return render_template('categoria_recovery.html', categorias=categorias)  # Crie esse template
